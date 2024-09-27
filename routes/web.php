853065f63822931bd52a41774e09f408f56e44b4
Ivan Marroquin <bryan.marroquin@coop1.com.sv>
2024-09-27 17:26:50 -0600

    login

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,19 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Rutas Web
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| Aquí es donde puedes registrar las rutas web para tu aplicación. Estas
+| rutas se cargan a través del RouteServiceProvider y todas se asignarán
+| al grupo de middleware "web". ¡Haz algo genial!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
