d3885f8b60dfbf7c25260929997b298c1ab53e3b
Your Name <you@example.com>
2024-10-01 06:07:43 -0600

    video 11

--- resources/views/admin/empresas/create.blade.php
+++ resources/views/admin/empresas/create.blade.php
@@ -1,11 +1,11 @@
 @extends('adminlte::master')
 
-@php( $dashboard_url = View::getSection('dashboard_url') ?? config('adminlte.dashboard_url', 'home') )
+@php($dashboard_url = View::getSection('dashboard_url') ?? config('adminlte.dashboard_url', 'home'))
 
 @if (config('adminlte.use_route_url', false))
-    @php( $dashboard_url = $dashboard_url ? route($dashboard_url) : '' )
+    @php($dashboard_url = $dashboard_url ? route($dashboard_url) : '')
 @else
-    @php( $dashboard_url = $dashboard_url ? url($dashboard_url) : '' )
+    @php($dashboard_url = $dashboard_url ? url($dashboard_url) : '')
 @endif
 
 @section('adminlte_css')
@@ -16,24 +16,143 @@
 @section('classes_body'){{ ($auth_type ?? 'login') . '-page' }}@stop
 
 @section('body')
-    <div class="{{ $auth_type ?? 'login' }}-box">
+    <div class="container">
+
+        
+        <center>
+            <img src="{{ asset('/images/logo.png') }}" width="250px" alt="">
+        </center>
+        <br>
 
         <div class="row">
             <div class="col-md-12">
-                <div class="card " style="box-shadow: 5px 5px 5px 5px #cccccc">
+                {{-- Card Box --}}
+                <div class="card {{ config('adminlte.classes_auth_card', 'card-outline card-primary') }}">
 
-                    {{-- Card Header --}}
-                    @hasSection('auth_header')
-                        <div class="card-header {{ config('adminlte.classes_auth_header', '') }}">
-                            <h3 class="card-title float-none text-center">
-                                @yield('auth_header')
-                            </h3>
-                        </div>
-                    @endif
+
+                    <div class="card-header {{ config('adminlte.classes_auth_header', '') }}">
+                        <h3 class="card-title float-none text-center">
+                            <b> Registro de una nueva empresa</b>
+                        </h3>
+                    </div>
 
                     {{-- Card Body --}}
-                    <div class="card-body {{ $auth_type ?? 'login' }}-card-body {{ config('adminlte.classes_auth_body', '') }}">
-                        @yield('auth_body')
+                    <div
+                        class="card-body {{ $auth_type ?? 'login' }}-card-body {{ config('adminlte.classes_auth_body', '') }}">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="logo">Logo</label>
+                                        <input type="file" class="form-control ">
+                                    </div>
+
+                                </div>
+                                <div class="col-md-9">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label for="pais">Pais</label>
+                                                <select name="" id="" class="form-control">
+                                                    <option value="Bolivia">Bolivia</option>
+                                                    <option value="El salvador">El salvador</option>
+                                                    <option value="Argentina">Argentina</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label for="nombre_empresa">Nombre de la empresa</label>
+                                                <input type="text" class="form-control">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label for="tipo_empresa">Tipo de empresa</label>
+                                                <input type="text" class="form-control">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label for="nit">NIT</label>
+                                                <input type="text" class="form-control">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label for="telefono">Telefonos de la empresa</label>
+                                                <input type="text" class="form-control">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label for="correo">Correo de la empresa</label>
+                                                <input type="email" class="form-control">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label for="cantidad_impuesto">Cantidad de impuesto</label>
+                                                <input type="number" class="form-control">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label for="nombre_impuesto">Nombre del impuesto</label>
+                                                <input type="text" class="form-control">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label for="moneda">Moneda</label>
+                                               <select name="" id="" class="form-control">
+                                                <option value="Bs.">Bs</option>
+                                                <option value="$">$</option>
+                                               </select>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label for="direccion">Dirección</label>
+                                                <input type="address" class="form-control">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label for="ciudad">Ciudad</label>
+                                                <input type="text" class="form-control">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label for="Departamento">Moneda</label>
+                                               <input type="text" class="form-control">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label for="codigo_postal">Codigo postal</label>
+                                                <input type="number" class="form-control">
+                                            </div>
+                                        </div>                                       
+                                    </div>                                                                                                         
+                                </div>
+
+                            </div>
+                        </form>
                     </div>
 
                     {{-- Card Footer --}}
